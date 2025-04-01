998416d766a4f3155e3d4e6e2df6ada45a8b3aa2
Ricardo Tanaka Terukina <ricardotanaka-externo@tgacompany.com>
2025-04-01 17:00:57 -0500

    Added some dialogs

--- Menu.cs
+++ Menu.cs
@@ -13,6 +13,7 @@ namespace Taller2DVirtual251
         public void Execute()
         {
             CreatePlayer();
+            ShowOptions();
         } 
 
         private void CreatePlayer()
@@ -21,7 +22,37 @@ namespace Taller2DVirtual251
             string name = Console.ReadLine();
             player = new Player(name);
 
-            Console.WriteLine($"Se creó con éxito a {player.Name}");
+            Console.WriteLine($"Se creó con éxito a {player.Name} y tiene {player.Life} de vida");
+        }
+
+        private void ShowOptions()
+        {
+            bool continueFlag = true;
+
+            while (continueFlag)
+            {
+                Console.WriteLine($"Nombre: {player.Name} - Vida: {player.Life}");
+                Console.WriteLine("1. Tomar poción");
+                Console.WriteLine("2. Agarrar el cactus con la mano");
+
+                string option= Console.ReadLine();
+                switch(option)
+                {
+                    case "1":
+                        player.Heal(10);
+                        Console.WriteLine($"{player.Name} se curó 10 de vida");
+                        break;
+                    case "2":
+                        player.TakeDamage(5);
+                        Console.WriteLine($"{player.Name} recibió 5 de daño");
+                        break;
+                }
+                if (player.Life <= 0)
+                {
+                    continueFlag = false;
+                }
+            }
+            Console.WriteLine("Moriste");
         }
     }
 }
--- Player.cs
+++ Player.cs
@@ -9,12 +9,34 @@ namespace Taller2DVirtual251
     internal class Player
     {
         private string name;
+        private int life;
 
         public string Name { get { return name; } }
+        public int Life {  get { return life; } }
 
         public Player(string name)
         {
             this.name = name;
+            life = 100;
+        }
+
+        private void ChangeLife(int value)
+        {
+            life += value;
+            if (life < 0)
+            {
+                life = 0;
+            }
+        }
+
+        public void TakeDamage(int value)
+        {
+            ChangeLife(-value);
+        }
+
+        public void Heal(int value)
+        {
+            ChangeLife(value);
         }
 
     }
