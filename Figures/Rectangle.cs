e6ac4b96ed3124ca895741823bd164c3973078ad
Ricardo Tanaka Terukina <ricardotanaka-externo@tgacompany.com>
2025-04-01 17:37:59 -0500

    Added rectangle example

---
+++ Figures/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller2DVirtual251.Figures
+{
+    internal class Rectangle
+    {
+        private float b;
+        private float h;
+
+        public Rectangle(float b, float h)
+        {
+            this.b = b;
+            this.h = h;
+        }
+
+        public float GetArea()
+        {
+            return b * h;
+        }
+    }
+}
