d15e78386b2a8182b5f06d3f35fe19c77769d1e9
Ricardo Tanaka Terukina <ricardotanaka-externo@tgacompany.com>
2025-04-01 18:48:17 -0500

    Added examples

---
+++ Classroom/Classroom.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller2DVirtual251.Classroom
+{
+    internal class Classroom
+    {
+        private string name;
+        private List<Student> students;
+
+        public Classroom(string name)
+        {
+            this.name = name;
+            students = new List<Student>();
+        }
+
+        public void AddStudent(Student student)
+        {
+            students.Add(student);
+        }
+
+        public void RemoveStudent(Student student)
+        {
+            students.Remove(student);
+        }
+
+        public int GetPassedAmount()
+        {
+            int count = 0;
+            foreach (Student student in students)
+            {
+                if (student.GetAverage() >= 13)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public int GetFailedAmount()
+        {
+            return students.Count - GetPassedAmount();
+        }
+
+        public List<Student> GetPassedStudents()
+        {
+            List<Student> s = new List<Student>();
+            foreach (Student student in students)
+            {
+                if (student.GetAverage() >= 13)
+                {
+                    s.Add(student);
+                }
+            }
+            return s;
+        }
+
+        public List<Student> GetFailedStudents()
+        {
+            List<Student> s = new List<Student>();
+            foreach (Student student in students)
+            {
+                if (student.GetAverage() < 13)
+                {
+                    s.Add(student);
+                }
+            }
+            return s;
+        }
+
+        public float GetAverage()
+        {
+            float sum = 0;
+            foreach (Student student in students)
+            {
+                sum += student.GetAverage();
+            }
+            return sum / students.Count;
+        }
+
+    }
+}
---
+++ Classroom/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller2DVirtual251.Classroom
+{
+    internal class Student
+    {
+        private string name;
+        private float grade1;
+        private float grade2;
+        private float grade3;
+
+        public Student(string name, float grade1, float grade2, float grade3)
+        {
+            this.name = name;
+            this.grade1 = grade1;
+            this.grade2 = grade2;
+            this.grade3 = grade3;
+        }
+
+        public float GetAverage()
+        {
+            return (grade1 + grade2 + 2 * grade3) / 4;
+        }
+    }
+}
